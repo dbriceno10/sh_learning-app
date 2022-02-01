276701b9786dcf9394e56a9b82b7bed78b93755d
David-j787 <davidpatejo@gmail.com>
2022-02-01 18:37:31 -0300

    components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import Landing from './Components/Landing';
 
 function App() {
+
+
+  // copy and paste for new component route ⬇⬇⬇⬇⬇
+  //     <Route path = '' element = {</>}></Route>
+  //    or <Route path = '/' element = {</>}/>
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+      <Route exact path = '/' element = {<Landing/>}/> 
+    </Routes>
+  )
 }
 
 export default App;
