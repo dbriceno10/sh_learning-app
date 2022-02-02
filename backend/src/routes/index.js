77f2b488abc3f8823484a3516d284cc11a4ba99a
Daniel Briceño <daniel.b100726@gmail.com>
2022-02-02 15:01:36 -0400

    Haciendo coneciones de prueba para la base de datos

---
+++ backend/src/routes/index.js
@@ -0,0 +1,14 @@
+const { Router } = require('express');
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+// const pokemons = require('./pokemons.js');
+// const types = require('./types.js');
+
+const router = Router();
+
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+// router.use('/pokemons', pokemons);
+// router.use('/types', types);
+
+module.exports = router;
