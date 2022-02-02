d5ccc66a74b442b25c9c6980031593529a4a17da
Samuel <samuelcantos18@gmail.com>
2022-02-02 12:38:52 -0300

    login Google

--- frontend/src/Components/Logout.js
+++ frontend/src/Components/Logout.js
@@ -5,9 +5,7 @@ import { useAuth0 } from '@auth0/auth0-react'
 export default function LogoutButton(){
     const {logout} = useAuth0();
 
-    return (
-        
-
+    return (        
         <button type='button' onClick={() => logout({returnTo: window.location.origin})}>Logout</button>
     )
 }
