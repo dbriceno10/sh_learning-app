404de8f3d1edab3b5d7627df050eb04251f5d4c0
Romina Acevedo <rominaace44@gmail.com>
2022-02-04 11:39:38 -0300

    actualizacion

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom';
-import Landing from './Components/Landing';
 import FormRegister from './Components/FormRegister';
 
 
@@ -14,7 +13,6 @@ function App() {
 
   return (
     <Routes>
-       <Route exact path = '/' element = {<Landing/>}/> 
        <Route exact path = '/signUp' element = {<FormRegister/>}/>  
 
     </Routes>
