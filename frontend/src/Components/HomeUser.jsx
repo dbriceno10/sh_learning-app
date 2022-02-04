75cccf74f2c9f41f9c767e45e674a35183736ffe
Nestoralons <nestoralons42@gmail.com>
2022-02-04 08:53:35 -0400

    HomeUser/creaciondelcomponente

---
+++ frontend/src/Components/HomeUser.jsx
@@ -0,0 +1,19 @@
+import NavHomeUser from "./NavHomeUser"
+import styles from './HomeUser.module.css'
+export default function HomeUser(){
+
+
+    
+    return (
+        <div className={styles.container} >
+            <section className={styles.seccion}>
+                <NavHomeUser/>
+            </section>
+            <section>
+                
+            </section>
+        </div>
+
+    )
+
+}
