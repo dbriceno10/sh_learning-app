d4e9382636bbf30a089ace5ab5548887c1488400
Marcelo Malacalza <MalacalzaMarcelo@gmail.com>
2022-02-05 17:04:07 -0300

    dejar el profile para el sprint 2

--- frontend/src/Components/Profile.js
+++ frontend/src/Components/Profile.js
@@ -1,17 +1,16 @@
 import React from "react";
+import "./Profile.css";
 
+export default function Profile() {
+	let user = JSON.parse(localStorage.getItem("user"));
 
-export default function Profile (){
-    
-    let user = JSON.parse(localStorage.getItem("user"));
-
-    return (
-        <div>
-            <h2>
-              {user.email}
-              {user.firstName}
-              {user.image}
-            </h2>
-        </div>
-    )
+	return (
+		<div>
+				<h3 style={{textAlign: "center"}}>¡Bienvenido a tu Perfil {user.firstName}!</h3>
+			<div className="imageName-container">
+				<img src={user.image ? user.image : ""} alt={user.firstName} className="profileImage"/>
+        <h4>{user.firstName} {user.lastName}</h4>
+			</div>
+		</div>
+	);
 }
