5ea9e40f2fdca4698086cadc1e91e6396bfa3fcd
Daniel Briceño <daniel.b100726@gmail.com>
2022-02-09 18:09:47 -0400

    bug fixes en courses

--- backend/src/routes/controller/review/postReview.js
+++ backend/src/routes/controller/review/postReview.js
@@ -1,18 +1,24 @@
-const { Course,Student,Review } = require("../../../db.js");
+const { Course, Student, Review } = require("../../../db.js");
 
-const postReview = async (req,res) => {
-
-    const { nameCourse, emailStudent, score } = req.body;
+const postReview = async (req, res) => {
+  const { nameCourse, emailStudent, score } = req.body;
+  try {
     const FKCourse = await Course.findOne({
       where: {
         name: nameCourse,
       },
     });
+    if (!FKCourse) {
+      return res.status(404).send({ message: "El curso no existe" });
+    }
     const FKStudent = await Student.findOne({
       where: {
         email: emailStudent,
       },
     });
+    if (!FKStudent) {
+      return res.status(404).send({ message: "El estudiante no existe" });
+    }
     const flag = await Review.findOne({
       where: {
         FKstudentID: FKStudent.id,
@@ -28,16 +34,18 @@ const postReview = async (req,res) => {
           FKstudentID: FKStudent.id,
           FKcourseID: FKCourse.id,
         });
-  
         res.status(200).send(review);
       } catch (error) {
         res.status(404).send(error);
       }
     } else {
-      res.status(404).send("Ya has calificado este curso");
+      res.status(404).send({ message: "Ya has calificado este curso" });
     }
-}
+  } catch (error) {
+    res.status(404).send(error);
+  }
+};
 
-module.exports={
-    postReview,
-}
+module.exports = {
+  postReview,
+};
