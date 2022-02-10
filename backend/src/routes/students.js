47c72ef5ca7707fbcb5a85a1c09a7c4238cd73ba
Daniel Briceño <daniel.b100726@gmail.com>
2022-02-10 18:12:30 -0400

    Creadas las rutas de administrador para buscar, actualizar y eliminar estudiantes y profesores

--- backend/src/routes/students.js
+++ backend/src/routes/students.js
@@ -2,8 +2,16 @@ const { Router } = require("express");
 
 const router = Router();
 
-const {getStudents} = require('./controller/students/getStudents')
+const {
+  getStudents,
+  getStudent,
+  updateStudent,
+  deteteStudent,
+} = require("./controller/students/getStudents");
 
-router.get('/', getStudents); 
+router.get("/", getStudents);
+router.get("/:id", getStudent);
+router.put("/:id", updateStudent);
+router.delete("/:id", deteteStudent);
 
 module.exports = router;
