6220babc5ce1e15a0c7147cc367de5f9da0b9ba2
Romina Acevedo <rominaace44@gmail.com>
2022-02-14 13:22:38 -0300

    prueba

--- backend/src/routes/controller/students/get.students.controller.js
+++ backend/src/routes/controller/students/get.students.controller.js
@@ -3,7 +3,7 @@ const { Student } = require("../../../db.js");
 const getStudents = async (req, res) => {
   try {
     let students = await Student.findAll({
-      attributes: ["id", "name", "lastName", "email", "avatar", "role"], //solo vamos a enviar estos atributos al front
+      attributes: ["id", "name", "lastName", "email", "avatar"], //solo vamos a enviar estos atributos al front
     });
     res.status(200).json(students);
   } catch (err) {
