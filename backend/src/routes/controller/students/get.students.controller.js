098383223e57ce9f4446cf48e5d8ad3b567f8cc8
Daniel Briceño <daniel.b100726@gmail.com>
2022-02-14 11:50:38 -0400

    bug fixes

--- backend/src/routes/controller/students/get.students.controller.js
+++ backend/src/routes/controller/students/get.students.controller.js
@@ -3,9 +3,11 @@ const { Student } = require("../../../db.js");
 const getStudents = async (req, res) => {
   try {
     let students = await Student.findAll({
-      attributes: ["id", "name", "lastName", "email", "avatar", "role"], //solo vamos a enviar estos atributos al front
+      // attributes: ["id", "name", "lastName", "email", "avatar", "role"], //solo vamos a enviar estos atributos al front
+      attributes: ["id", "name", "lastName", "email", "avatar"],
     });
-    res.status(200).json(students);
+    // res.status(200).json(students);
+    res.status(200).send({id: student.id, name: student.name, lastName: student.lastName, email: student.email, avatar: student.avatar, role: "alumno"})
   } catch (err) {
     console.error(err);
     res.status(404).send({ message: "Error al obtener los estudiantes" });
@@ -19,9 +21,11 @@ const getStudent = async (req, res) => {
       where: {
         id: id,
       },
-      attributes: ["id", "name", "lastName", "email", "avatar", "role"], //solo vamos a enviar estos atributos al front
+      // attributes: ["id", "name", "lastName", "email", "avatar", "role"], //solo vamos a enviar estos atributos al front
+      attributes: ["id", "name", "lastName", "email", "avatar"],
     });
-    res.status(200).json(student);
+    // res.status(200).json(student);
+    res.status(200).send({id: student.id, name: student.name, lastName: student.lastName, email: student.email, avatar: student.avatar, role: "alumno"})
   } catch (err) {
     console.error(err);
     res.status(404).send({ message: "Error al obtener el estudiante" });
