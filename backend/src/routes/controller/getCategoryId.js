9950b6d1d6c742d8758f0aecdbd73d2b29a432f9
Daniel Briceño <daniel.b100726@gmail.com>
2022-02-13 23:13:55 -0400

    getCategoryId ahora las categorías se buscan en minúscula

--- backend/src/routes/controller/getCategoryId.js
+++ backend/src/routes/controller/getCategoryId.js
@@ -7,7 +7,7 @@ async function getCategoryId(arrayCategory) { //Recibe un array de categorias
   for (let i = 0; i < arrayCategory.length; i++) { //Recorre el array de categorias
     categories.push( //Agrega el id de la categoria
       await Category.findOne({
-        where: { name: arrayCategory[i] }, //Busca la categoria
+        where: { name: arrayCategory[i].trim().toLowerCase() }, //Busca la categoria
         attributes: ["id"], //saco el atributo id
       })
     );
